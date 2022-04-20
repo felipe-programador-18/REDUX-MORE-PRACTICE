0f1ae2bf24a953d28373cad1492903ec6f239106
Felipe <machadofelipe2016@outlook.com>
2022-04-20 15:55:51 -0300

    c4-removed component

--- practice-redux/src/counter.js
+++ practice-redux/src/counter.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import {decrement, increment} from './actoins'
 // rewrite with components classes!!
